be50cd8bf8a8937a287d3e47ef0bce4d6b44d76c
alex <843885081@qq.com>
2019-01-09 19:38:43 +0800

    使用最原始的模型测试

--- keras_imp/original_model.py
+++ keras_imp/original_model.py
@@ -646,71 +646,71 @@ if stage == 'train':
             ampl = max(1.0, 100 ** -0.1 * ampl)
         model.save('standard_train_10epochs.model')
 
-    if os.path.isfile('/home/zhangjie/KWhaleData/piotte/mpiotte-standard.model'):
-        tmp = keras.models.load_model('/home/zhangjie/KWhaleData/piotte/mpiotte-standard.model')
-        model.set_weights(tmp.get_weights())
+if os.path.isfile('/home/zhangjie/KWhaleData/piotte/mpiotte-standard.model'):
+    tmp = keras.models.load_model('/home/zhangjie/KWhaleData/piotte/mpiotte-standard.model')
+    model.set_weights(tmp.get_weights())
 
 
-    def prepare_submission(threshold, filename):
-        """
-        Generate a Kaggle submission file.
-        @param threshold the score given to 'new_whale'
-        @param filename the submission file name
-        """
-        vtop = 0
-        vhigh = 0
-        pos = [0, 0, 0, 0, 0, 0]
-        with open(filename, 'wt', newline='\n') as f:
-            f.write('Image,Id\n')
-            for i, p in enumerate(tqdm(submit)):
-                t = []
-                s = set()
-                a = score[i, :]
-                for j in list(reversed(np.argsort(a))):
-                    h = known[j]
-                    if a[j] < threshold and new_whale not in s:
-                        pos[len(t)] += 1
-                        s.add(new_whale)
-                        t.append(new_whale)
-                        if len(t) == 5: break;
-                    for w in h2ws[h]:
-                        assert w != new_whale
-                        if w not in s:
-                            if a[j] > 1.0:
-                                vtop += 1
-                            elif a[j] >= threshold:
-                                vhigh += 1
-                            s.add(w)
-                            t.append(w)
-                            if len(t) == 5: break;
+def prepare_submission(threshold, filename):
+    """
+    Generate a Kaggle submission file.
+    @param threshold the score given to 'new_whale'
+    @param filename the submission file name
+    """
+    vtop = 0
+    vhigh = 0
+    pos = [0, 0, 0, 0, 0, 0]
+    with open(filename, 'wt', newline='\n') as f:
+        f.write('Image,Id\n')
+        for i, p in enumerate(tqdm(submit)):
+            t = []
+            s = set()
+            a = score[i, :]
+            for j in list(reversed(np.argsort(a))):
+                h = known[j]
+                if a[j] < threshold and new_whale not in s:
+                    pos[len(t)] += 1
+                    s.add(new_whale)
+                    t.append(new_whale)
                     if len(t) == 5: break;
-                if new_whale not in s: pos[5] += 1
-                assert len(t) == 5 and len(s) == 5
-                f.write(p + ',' + ' '.join(t[:5]) + '\n')
-        return vtop, vhigh, pos
+                for w in h2ws[h]:
+                    assert w != new_whale
+                    if w not in s:
+                        if a[j] > 1.0:
+                            vtop += 1
+                        elif a[j] >= threshold:
+                            vhigh += 1
+                        s.add(w)
+                        t.append(w)
+                        if len(t) == 5: break;
+                if len(t) == 5: break;
+            if new_whale not in s: pos[5] += 1
+            assert len(t) == 5 and len(s) == 5
+            f.write(p + ',' + ' '.join(t[:5]) + '\n')
+    return vtop, vhigh, pos
 
 
-    # Find elements from training sets not 'new_whale'
-    tic = time.time()
-    h2ws = {}
-    for p, w in tagged.items():
-        if w != new_whale:  # Use only identified whales
-            h = p2h[p]
-            if h not in h2ws: h2ws[h] = []
-            if w not in h2ws[h]: h2ws[h].append(w)
-    known = sorted(list(h2ws.keys()))
+# Find elements from training sets not 'new_whale'
+tic = time.time()
+h2ws = {}
+for p, w in tagged.items():
+    if w != new_whale:  # Use only identified whales
+        h = p2h[p]
+        if h not in h2ws: h2ws[h] = []
+        if w not in h2ws[h]: h2ws[h].append(w)
+known = sorted(list(h2ws.keys()))
 
-    # Dictionary of picture indices
-    h2i = {}
-    for i, h in enumerate(known): h2i[h] = i
+# Dictionary of picture indices
+h2i = {}
+for i, h in enumerate(known): h2i[h] = i
 
-    # Evaluate the model.
-    fknown = branch_model.predict_generator(FeatureGen(known), max_queue_size=20, workers=10, verbose=0)
-    fsubmit = branch_model.predict_generator(FeatureGen(submit), max_queue_size=20, workers=10, verbose=0)
-    score = head_model.predict_generator(ScoreGen(fknown, fsubmit), max_queue_size=20, workers=10, verbose=0)
-    score = score_reshape(score, fknown, fsubmit)
+# Evaluate the model.
+fknown = branch_model.predict_generator(FeatureGen(known), max_queue_size=20, workers=10, verbose=0)
+fsubmit = branch_model.predict_generator(FeatureGen(submit), max_queue_size=20, workers=10, verbose=0)
+score = head_model.predict_generator(ScoreGen(fknown, fsubmit), max_queue_size=20, workers=10, verbose=0)
+score = score_reshape(score, fknown, fsubmit)
 
-    # Generate the subsmission file.
-    prepare_submission(0.99, 'submission.csv')
-    toc = time.time()
-    print("Submission time: ", (toc - tic) / 60.)
+# Generate the subsmission file.
+prepare_submission(0.99, 'submission.csv')
+toc = time.time()
+print("Submission time: ", (toc - tic) / 60.)

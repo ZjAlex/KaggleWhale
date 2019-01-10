a9a3128805a740cbbe8ad9c5ac1b65d3bf6d9c1e
alex <843885081@qq.com>
2019-01-10 10:11:19 +0800

    train 20 epochs

--- keras_imp/original_model.py
+++ keras_imp/original_model.py
@@ -553,8 +553,8 @@ if stage == 'train':
             print('noise ampl.  = ', ampl)
             make_steps(5, ampl)
             ampl = max(1.0, 100 ** -0.1 * ampl)
-            # epoch -> 150
-            for _ in range(5): make_steps(5, 1.0)
+            # # epoch -> 150
+            # for _ in range(5): make_steps(5, 1.0)
         #     # epoch -> 200
         #     set_lr(model, 16e-5)
         #     for _ in range(10): make_steps(5, 0.5)
@@ -565,23 +565,23 @@ if stage == 'train':
         #     set_lr(model, 1e-5)
         #     for _ in range(2): make_steps(5, 0.25)
         #     # epoch -> 300
-            weights = model.get_weights()
-            model, branch_model, head_model = build_model(64e-5, 0.0002)
-            model.set_weights(weights)
-            for _ in range(2): make_steps(5, 1.0)
-            # epoch -> 350
-            set_lr(model, 16e-5)
-            for _ in range(2): make_steps(5, 0.5)
+        #     weights = model.get_weights()
+        #     model, branch_model, head_model = build_model(64e-5, 0.0002)
+        #     model.set_weights(weights)
+        #     for _ in range(2): make_steps(5, 1.0)
+        #     # epoch -> 350
+        #     set_lr(model, 16e-5)
+        #     for _ in range(2): make_steps(5, 0.5)
         #     # epoch -> 390
         #     set_lr(model, 4e-5)
         #     for _ in range(8): make_steps(5, 0.25)
         #     # epoch -> 400
         #     set_lr(model, 1e-5)
         #     for _ in range(2): make_steps(5, 0.25)
-        model.save_weights('attention_model_weights.h5')
+        model.save_weights('attention_20epochs_model_weights.h5')
 
-if os.path.isfile('/home/zhangjie/KaggleWhale/attention_model_weights.h5'):
-    model.load_weights('/home/zhangjie/KaggleWhale/attention_model_weights.h5', by_name=True, skip_mismatch=True)
+if os.path.isfile('/home/zhangjie/KaggleWhale/attention_20epochs_model_weights.h5'):
+    model.load_weights('/home/zhangjie/KaggleWhale/attention_20epochs_model_weights.h5', by_name=True, skip_mismatch=True)
     #model.set_weights(tmp.get_weights())
 
 

f8ecf0b4dd580d37e1bed65495685d488c845532
alex <843885081@qq.com>
2019-01-12 16:22:28 +0800

    modify

--- modified_version/train.py
+++ modified_version/train.py
@@ -1,39 +1,7 @@
-import gzip
-# Read or generate p2h, a dictionary of image name to image id (picture to hash)
-import pickle
-import platform
-import random
-# Suppress annoying stderr output when importing keras.
-import sys
-from lap import lapjv
-from math import sqrt
-# Determine the size of each image
-from os.path import isfile
-
-import keras
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from PIL import Image as pil_image
-from imagehash import phash
-from keras import backend as K
-from keras import regularizers
-from keras.engine.topology import Input
-from keras.layers import Activation, Add, BatchNormalization, Concatenate, Conv2D, Dense, Flatten, GlobalMaxPooling2D, \
-    Lambda, MaxPooling2D, Reshape
-from keras.models import Model
-from keras.optimizers import Adam
-from keras.preprocessing.image import img_to_array
-from keras.utils import Sequence
-from pandas import read_csv
-from scipy.ndimage import affine_transform
-from tqdm import tqdm
 import time
 import os
 import sys
-from Model import build_model
 from keras.callbacks import Callback
-
 from attention_model import build_model
 from toolFuncs import *
 

9d2e9cb1e14008179fa91322ef0d30385d79f747
alex <843885081@qq.com>
2019-01-14 10:33:19 +0800

    add softmax

--- modified_version/train.py
+++ modified_version/train.py
@@ -385,17 +385,21 @@ if True:
             print('noise ampl.  = ', ampl)
             make_steps(5, ampl)
             ampl = max(1.0, 100 ** -0.1 * ampl)
+        model.save_weights('/home/zhangjie/KWhaleData/attention_' + args.output_path + '_model_weights.h5')
         # epoch -> 150
         for _ in range(9): make_steps(5, 1.0)
+        model.save_weights('/home/zhangjie/KWhaleData/attention_' + args.output_path + '_model_weights.h5')
         # epoch -> 200
         set_lr(model, 16e-5)
         for _ in range(5): make_steps(5, 0.5)
+        model.save_weights('/home/zhangjie/KWhaleData/attention_' + args.output_path + '_model_weights.h5')
         # epoch -> 240
         set_lr(model, 4e-5)
         for _ in range(4): make_steps(5, 0.25)
         # epoch -> 250
         set_lr(model, 1e-5)
         for _ in range(1): make_steps(5, 0.25)
+        model.save_weights('/home/zhangjie/KWhaleData/attention_' + args.output_path + '_model_weights.h5')
         # epoch -> 300
         weights = model.get_weights()
         model, branch_model, head_model = build_model(64e-5, 0.0002)
@@ -404,6 +408,7 @@ if True:
         # epoch -> 350
         set_lr(model, 16e-5)
         for _ in range(5): make_steps(5, 0.5)
+        model.save_weights('/home/zhangjie/KWhaleData/attention_' + args.output_path + '_model_weights.h5')
         # epoch -> 390
         set_lr(model, 4e-5)
         for _ in range(4): make_steps(5, 0.25)
